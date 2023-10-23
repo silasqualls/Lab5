19c0409b5230f1bfd3d9d883fc22defd17956c5c
silasqualls <silas.qualls@maine.edu>
2023-10-23 14:12:39 -0400

    Update BinarySearchTree.java

--- src/lab5package/BinarySearchTree.java
+++ src/lab5package/BinarySearchTree.java
@@ -1,9 +1,17 @@
 package lab5package;
 
-public abstract class BinarySearchTree<Integer> {
-public abstract boolean add(int data) {
+public abstract class BinarySearchTree {
+   
+    public class Node{
+        int size;
+        Node root;
+    }
+    
+    public abstract boolean add(int data) {
     
 }
-public int getSize() {return size;}
+public int getSize() {
+    return size;
+    }
 
 }
